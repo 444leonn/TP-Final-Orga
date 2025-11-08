76f1c134ac1f05a35624adbc97ee10aed232dfd3
444leonn <acostaleon25@gmail.com>
2025-11-08 15:53:37 -0300

    add: constantes

---
+++ src/constantes.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTES_H_
+#define CONSTANTES_H_
+
+#define ARCH_BIN "./inputBinario.bin"
+#define ARCH_TXT "outputTexto.txt"
+#define MODO_LECTURA "r"
+#define MODO_ESCRITURA "w"
+#define ERROR_APERTURA "Error, al abrir archivo."
+#define EXITO_APERTURA "Archivos Abiertos correctamente."
+#define ERROR -1
+
+#endif // CONSTANTES_H_
